81ff03f45e7c060bddbc3b03124919a7e9850789
Pantilimon <Pantilimon@users.noreply.github.com>
2019-12-25 11:24:47 +0500

    Yes

--- exp.py
+++ exp.py
@@ -1,20 +1,20 @@
+"""Задачка по ряду Тейлора"""
 import math
 
-iterations = 50
+ITERATIONS = 50
 
-def my_exp(x):
+def my_exp(x_0: float):
     """Вычисление экспоненты при помощи частичного суммирования ряда Тейлора"""
-    x_pow = 1
-    multiplier = 1
-    partial_sum = 1
-    for n in range(1, iterations):
-        x_pow *= x  # 
-        multiplier *= 1 / (n) # 
+    x_pow: int = 1
+    multiplier: float = 1
+    partial_sum: float = 1
+    for n_0 in range(1, ITERATIONS):
+        x_pow *= x_0
+        multiplier *= 1 / (n_0)
         partial_sum += x_pow * multiplier
-    
     return partial_sum
 
 
-a = float(input())
+a: float = float(input())
 print(my_exp(a))
 print(math.e**(a))
