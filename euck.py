71506590011deb42961408450c1bce4220cb3809
Pantilimon <55803231+Pantilimon@users.noreply.github.com>
2020-08-28 15:01:39 +0500

    Delete euck.py

--- euck.py
+++
@@ -1,8 +0,0 @@
-def eucl_greatestcommondivisor(a:int,b:int)->int:
-    if(b==0):
-        return a
-    return eucl_greatestcommondivisor(b,a%b)
-a = int(input())
-b = int(input())
-
-print(eucl_greatestcommondivisor(a,b))
